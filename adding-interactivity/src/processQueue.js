577941b530f56497a9b08ca439cdefeeca1d3499
Seongmin <chr92@naver.com>
2025-02-26 14:58:13 +0900

    feat: 2-5. Queueing a series of state updates - challenge2

---
+++ adding-interactivity/src/processQueue.js
@@ -0,0 +1,14 @@
+export function getFinalState(baseState, queue) {
+  let finalState = baseState;
+
+  // TODO: do something with the queue...
+  for (let update of queue) {
+    if (typeof update === "function") {
+      finalState = update(finalState);
+    } else {
+      finalState = update;
+    }
+  }
+
+  return finalState;
+}
