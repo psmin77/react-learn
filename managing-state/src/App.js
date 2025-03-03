b7fb03834c754bd67dae81ea920861bd8894c012
Seongmin <chr92@naver.com>
2025-03-03 18:46:13 +0900

    feat: 3-2. Choosing the state structure - challenge3

--- managing-state/src/App.js
+++ managing-state/src/App.js
@@ -1,51 +1,39 @@
 import { useState } from "react";
-import AddItem from "./AddItem.js";
-import PackingList from "./PackingList.js";
+import { initialLetters } from "./data.js";
+import Letter from "./Letter.js";
 
-let nextId = 3;
-const initialItems = [
-  { id: 0, title: "Warm socks", packed: true },
-  { id: 1, title: "Travel journal", packed: false },
-  { id: 2, title: "Watercolors", packed: false },
-];
+export default function MailClient() {
+  const [letters, setLetters] = useState(initialLetters);
+  const [highlightedId, setHighlightedId] = useState(null);
 
-export default function TravelPlan() {
-  const [items, setItems] = useState(initialItems);
-
-  const total = items.length;
-  const packed = items.filter((item) => item.packed).length;
-
-  function handleAddItem(title) {
-    setItems([
-      ...items,
-      {
-        id: nextId++,
-        title,
-        packed: false,
-      },
-    ]);
+  function handleHover(letterId) {
+    setHighlightedId(letterId);
   }
 
-  function handleChangeItem(nextItem) {
-    setItems(items.map((item) => (item.id === nextItem.id ? nextItem : item)));
-  }
-
-  function handleDeleteItem(itemId) {
-    setItems(items.filter((item) => item.id !== itemId));
+  function handleStar(starredId) {
+    setLetters(
+      letters.map((letter) =>
+        letter.id === starredId
+          ? { ...letter, isStarred: !letter.isStarred }
+          : letter
+      )
+    );
   }
 
   return (
     <>
-      <AddItem onAddItem={handleAddItem} />
-      <PackingList
-        items={items}
-        onChangeItem={handleChangeItem}
-        onDeleteItem={handleDeleteItem}
-      />
-      <hr />
-      <b>
-        {packed} out of {total} packed!
-      </b>
+      <h2>Inbox</h2>
+      <ul>
+        {letters.map((letter) => (
+          <Letter
+            key={letter.id}
+            letter={letter}
+            isHighlighted={letter.id === highlightedId}
+            onHover={handleHover}
+            onToggleStar={handleStar}
+          />
+        ))}
+      </ul>
     </>
   );
 }
