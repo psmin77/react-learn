559f1682f59f22d265b2ac9fbf94f68d975f73db
Seongmin <chr92@naver.com>
2025-03-05 23:03:47 +0900

    feat: 4-2. Manipulating the dom with refs - challenge2

--- escape-hatches/src/App.js
+++ escape-hatches/src/App.js
@@ -1,31 +1,21 @@
-import { useState, useRef } from 'react';
+import { useRef } from 'react';
 
-export default function VideoPlayer() {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const playingRef = useRef(null);
+export default function Page() {
+  const inputRef = useRef(null);
 
   function handleClick() {
-    const nextIsPlaying = !isPlaying;
-    setIsPlaying(nextIsPlaying);
-
-    if (nextIsPlaying) {
-      playingRef.current.play();
-    } else {
-      playingRef.current.pause();
-    }
+    inputRef.current.focus();
   }
 
   return (
     <>
-      <button onClick={handleClick}>
-        {isPlaying ? 'Pause' : 'Play'}
-      </button>
-      <video width="250" ref={playingRef}>
-        <source
-          src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-          type="video/mp4"
-        />
-      </video>
+      <nav>
+        <button onClick={handleClick}>Search</button>
+      </nav>
+      <input
+      ref={inputRef}
+        placeholder="Looking for something?"
+      />
     </>
-  )
+  );
 }
