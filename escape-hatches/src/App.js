0ccaae96fd4408d56f67b655e3c725eb2dedaf3e
Seongmin <chr92@naver.com>
2025-03-05 23:09:57 +0900

    feat: 4-2. Manipulating the dom with refs - challenge3

--- escape-hatches/src/App.js
+++ escape-hatches/src/App.js
@@ -1,21 +1,60 @@
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
+import { flushSync } from 'react-dom';
 
-export default function Page() {
-  const inputRef = useRef(null);
-
-  function handleClick() {
-    inputRef.current.focus();
-  }
-
+export default function CatFriends() {
+  const selectedRef = useRef(null);
+  const [index, setIndex] = useState(0);
   return (
     <>
       <nav>
-        <button onClick={handleClick}>Search</button>
+      <button onClick={() => {
+          flushSync(() => {
+            if (index < catList.length - 1) {
+              setIndex(index + 1);
+            } else {
+              setIndex(0);
+            }
+          });
+          selectedRef.current.scrollIntoView({
+            behavior: 'smooth',
+            block: 'nearest',
+            inline: 'center'
+          });
+        }}>
+          Next
+        </button>
       </nav>
-      <input
-      ref={inputRef}
-        placeholder="Looking for something?"
-      />
+      <div>
+        <ul>
+          {catList.map((cat, i) => (
+            <li
+            key={cat.id}
+            ref={index === i ?
+              selectedRef :
+              null
+            }
+          >
+              <img
+                className={
+                  index === i ?
+                    'active' :
+                    ''
+                }
+                src={cat.imageUrl}
+                alt={'Cat #' + cat.id}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
+}
+
+const catList = [];
+for (let i = 0; i < 10; i++) {
+  catList.push({
+    id: i,
+    imageUrl: 'https://loremflickr.com/250/200/cat?lock=' + i
+  });
 }
