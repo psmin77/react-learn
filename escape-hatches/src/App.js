8aef6a9ce59d41718ddfca53bc7274b03b5432a3
Seongmin <chr92@naver.com>
2025-03-05 15:44:43 +0900

    feat: 4-1. Referencing values with refs - challenge2

--- escape-hatches/src/App.js
+++ escape-hatches/src/App.js
@@ -1,34 +1,7 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
-export default function Chat() {
-  const [text, setText] = useState("");
-  const [isSending, setIsSending] = useState(false);
-  const timeoutRef = useRef(null);
+export default function Toggle() {
+  const [isOn, setIsOn] = useState(false);
 
-  function handleSend() {
-    setIsSending(true);
-    timeoutRef.current = setTimeout(() => {
-      alert("Sent!");
-      setIsSending(false);
-    }, 3000);
-  }
-
-  function handleUndo() {
-    setIsSending(false);
-    clearTimeout(timeoutRef.current);
-  }
-
-  return (
-    <>
-      <input
-        disabled={isSending}
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button disabled={isSending} onClick={handleSend}>
-        {isSending ? "Sending..." : "Send"}
-      </button>
-      {isSending && <button onClick={handleUndo}>Undo</button>}
-    </>
-  );
+  return <button onClick={() => setIsOn(!isOn)}>{isOn ? "On" : "Off"}</button>;
 }
