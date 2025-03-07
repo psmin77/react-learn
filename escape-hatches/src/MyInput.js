eb61214ed8c40c5b947823b237f2a3eaf5644e5a
Seongmin <chr92@naver.com>
2025-03-07 09:08:15 +0900

    feat: 4-3. Synchronizing with effects - challenge1

---
+++ escape-hatches/src/MyInput.js
@@ -0,0 +1,9 @@
+import { useEffect, useRef } from "react";
+
+export default function MyInput({ value, onChange }) {
+  const ref = useRef(null);
+
+  useEffect(() => ref.current.focus());
+
+  return <input ref={ref} value={value} onChange={onChange} />;
+}
