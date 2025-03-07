40555f3f574cc79314d47f0179a0f97087b459af
Seongmin <chr92@naver.com>
2025-03-07 09:15:59 +0900

    feat: 4-3. Synchronizing with effects - challenge2

--- escape-hatches/src/MyInput.js
+++ escape-hatches/src/MyInput.js
@@ -1,9 +1,12 @@
 import { useEffect, useRef } from "react";
 
-export default function MyInput({ value, onChange }) {
+export default function MyInput({ shouldFocus, value, onChange }) {
   const ref = useRef(null);
 
-  useEffect(() => ref.current.focus());
+  // TODO: shouldFocus가 true일때만 호출되도록
+  useEffect(() => {
+    if (shouldFocus) ref.current.focus();
+  }, [shouldFocus]);
 
   return <input ref={ref} value={value} onChange={onChange} />;
 }
