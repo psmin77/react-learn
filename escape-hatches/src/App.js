7895f75376ac456ad668fa3923af79220c5cc592
Seongmin <chr92@naver.com>
2025-03-09 20:52:24 +0900

    feat: 4-8. Reusing logic with custom hooks - challenge3

--- escape-hatches/src/App.js
+++ escape-hatches/src/App.js
@@ -2,23 +2,6 @@ import { useState } from 'react';
 import { useCounter } from './useCounter.js';
 
 export default function Counter() {
-  const [delay, setDelay] = useState(1000);
-  const count = useCounter(delay);
-  return (
-    <>
-      <label>
-        Tick duration: {delay} ms
-        <br />
-        <input
-          type="range"
-          value={delay}
-          min="10"
-          max="2000"
-          onChange={e => setDelay(Number(e.target.value))}
-        />
-      </label>
-      <hr />
-      <h1>Ticks: {count}</h1>
-    </>
-  );
+  const count = useCounter(1000);
+  return <h1>Seconds passed: {count}</h1>;
 }
