04c3642998f48a0072c1ce8354b2c0d235ba991b
Seongmin <chr92@naver.com>
2025-03-09 16:29:28 +0900

    feat: 4-7. Removing effect dependencies - challenge3

--- escape-hatches/src/ChatRoom.js
+++ escape-hatches/src/ChatRoom.js
@@ -1,67 +1,13 @@
-import { useState, useEffect } from 'react';
-import { fetchData } from './api.js';
+import { useEffect } from 'react';
+import { createConnection } from './chat.js';
 
-export default function Page() {
-  const [planetList, setPlanetList] = useState([])
-  const [planetId, setPlanetId] = useState('');
-
-  const [placeList, setPlaceList] = useState([]);
-  const [placeId, setPlaceId] = useState('');
-
+export default function ChatRoom({ options }) {
+  const { serverUrl, roomId } = options;
   useEffect(() => {
-    let ignore = false;
-    fetchData('/planets').then(result => {
-      if (!ignore) {
-        console.log('Fetched a list of planets.');
-        setPlanetList(result);
-        setPlanetId(result[0].id); // 첫 번째 행성을 선택합니다.
-      }
-    });
-    return () => {
-      ignore = true;
-    }
-  }, []);
+    const connection = createConnection({ serverUrl, roomId });
+    connection.connect();
+    return () => connection.disconnect();
+  }, [serverUrl, roomId]);
 
-  useEffect(() => {
-    let ignore = false;
-    if (planetId) {
-    fetchData('/planets/' + planetId + '/places').then(result => {
-      if (!ignore) {
-        console.log('Fetched a list of places on ' + planetId);
-        setPlaceList(result);
-        setPlaceId(result[0].id); // 첫 번째 장소를 선택합니다.
-      }
-    });
-  }
-    return () => {
-      ignore = true;
-    }
-  }, [planetId]);
-
-  return (
-    <>
-      <label>
-        Pick a planet:{' '}
-        <select value={planetId} onChange={e => {
-          setPlanetId(e.target.value);
-        }}>
-          {planetList.map(planet =>
-            <option key={planet.id} value={planet.id}>{planet.name}</option>
-          )}
-        </select>
-      </label>
-      <label>
-        Pick a place:{' '}
-        <select value={placeId} onChange={e => {
-          setPlaceId(e.target.value);
-        }}>
-          {placeList.map(place =>
-            <option key={place.id} value={place.id}>{place.name}</option>
-          )}
-        </select>
-      </label>
-      <hr />
-      <p>You are going to: {placeId || '???'} on {planetId || '???'} </p>
-    </>
-  );
+  return <h1>Welcome to the {options.roomId} room!</h1>;
 }
