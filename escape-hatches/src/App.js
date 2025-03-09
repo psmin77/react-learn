f4061f57e8596cf2888d9fcb2de622e85b4d97d5
Seongmin <chr92@naver.com>
2025-03-09 16:27:09 +0900

    feat: 4-7. Removing effect dependencies - challenge2

--- escape-hatches/src/App.js
+++ escape-hatches/src/App.js
@@ -1,19 +1,59 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { experimental_useEffectEvent as useEffectEvent } from 'react';
+import { FadeInAnimation } from './animation.js';
 
-export default function Timer() {
-  const [count, setCount] = useState(0);
+function Welcome({ duration }) {
+  const ref = useRef(null);
+
+  const onStart = useEffectEvent(() => {
+    const animation = new FadeInAnimation(ref.current);
+    animation.start(duration)
+  });
 
   useEffect(() => {
-    console.log('✅ Creating an interval');
-    const id = setInterval(() => {
-      console.log('⏰ Interval tick');
-      setCount(c => c + 1);
-    }, 1000);
-    return () => {
-      console.log('❌ Clearing an interval');
-      clearInterval(id);
-    };
+    onStart();
+    return () => animation.stop();
   }, []);
 
-  return <h1>Counter: {count}</h1>
+  return (
+    <h1
+      ref={ref}
+      style={{
+        opacity: 0,
+        color: 'white',
+        padding: 50,
+        textAlign: 'center',
+        fontSize: 50,
+        backgroundImage: 'radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)'
+      }}
+    >
+      Welcome
+    </h1>
+  );
+}
+
+export default function App() {
+  const [duration, setDuration] = useState(1000);
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <label>
+        <input
+          type="range"
+          min="100"
+          max="3000"
+          value={duration}
+          onChange={e => setDuration(Number(e.target.value))}
+        />
+        <br />
+        Fade in duration: {duration} ms
+      </label>
+      <button onClick={() => setShow(!show)}>
+        {show ? 'Remove' : 'Show'}
+      </button>
+      <hr />
+      {show && <Welcome duration={duration} />}
+    </>
+  );
 }
