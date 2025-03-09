86f2765bd1140e5afc463449da63fce5cc955ef7
Seongmin <chr92@naver.com>
2025-03-09 16:23:45 +0900

    feat: 4-7. Removing effect dependencies - challenge1

--- escape-hatches/src/App.js
+++ escape-hatches/src/App.js
@@ -1,58 +1,19 @@
 import { useState, useEffect } from 'react';
-import { experimental_useEffectEvent as useEffectEvent } from 'react';
-import { createConnection, sendMessage } from './chat.js';
-import { showNotification } from './notifications.js';
 
-const serverUrl = 'https://localhost:1234';
-
-function ChatRoom({ roomId, theme }) {
-  const onConnected = useEffectEvent((selectedRoom) => {
-    showNotification(selectedRoom + '에 오신 것을 환영합니다', theme);
-  });
+export default function Timer() {
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    const connection = createConnection(serverUrl, roomId);
-    connection.on('connected', () => {
-      setTimeout(() => {
-        onConnected(roomId);
-      }, 2000);
-    });
-    connection.connect();
-    return () => connection.disconnect();
-  }, [roomId]);
+    console.log('✅ Creating an interval');
+    const id = setInterval(() => {
+      console.log('⏰ Interval tick');
+      setCount(c => c + 1);
+    }, 1000);
+    return () => {
+      console.log('❌ Clearing an interval');
+      clearInterval(id);
+    };
+  }, []);
 
-  return <h1>{roomId} 방에 오신 것을 환영합니다!</h1>
-}
-
-export default function App() {
-  const [roomId, setRoomId] = useState('general');
-  const [isDark, setIsDark] = useState(false);
-  return (
-    <>
-      <label>
-        채팅방 선택:{' '}
-        <select
-          value={roomId}
-          onChange={e => setRoomId(e.target.value)}
-        >
-          <option value="general">general</option>
-          <option value="travel">travel</option>
-          <option value="music">music</option>
-        </select>
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          checked={isDark}
-          onChange={e => setIsDark(e.target.checked)}
-        />
-        어두운 테마 사용
-      </label>
-      <hr />
-      <ChatRoom
-        roomId={roomId}
-        theme={isDark ? 'dark' : 'light'}
-      />
-    </>
-  );
+  return <h1>Counter: {count}</h1>
 }
